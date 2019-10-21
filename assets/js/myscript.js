2f2e05866d513ca35787d2c213bac03a26afe37e
vonaxl <vonaxlcorso@gmail.com>
2019-10-21 17:15:56 +0200

    done email part

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,12 +1,17 @@
 var emailVerificato = ["a@gmail.com", "b@gmail.com", "c@gmail.com", "d@gmail.com", "e@gmail.com", ];
+var error;
 
 function verifica() {
   email = document.getElementById("emailInput").value;
   console.log(email);
   for (var i = 0; i < emailVerificato.length; i++) {
     if (email==emailVerificato[i]) {
-      alert("Sei invitato")
+      error=1;
     }
   }
-
+  if (error==1) {
+    alert("Sei invitato")
+  }else {
+    alert("Non sei invitato")
+  }
 }
